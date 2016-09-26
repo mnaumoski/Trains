01c6b17b13be1284fc09e64aee411a152c44fb71
mnaumoski <naumoski.marija@gmail.com>
2016-09-26 13:35:47 -0400

    update js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Trains
+
+This is an exciting list. Add trains and have it show how often they visit the station what it their starting time.
--- javascript/app.js
+++ javascript/app.js
@@ -1,85 +1,108 @@
-  // Initialize Firebase
-  var config = {
-      apiKey: "AIzaSyAv0YOCWtIxD64X0f1_bbAB08XyHuWH7Jk",
-      authDomain: "trainstation-2b3dd.firebaseapp.com",
-      databaseURL: "https://trainstation-2b3dd.firebaseio.com",
-      storageBucket: "trainstation-2b3dd.appspot.com",
-      messagingSenderId: "393068726925"
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-  console.log("test");
-  // var current = moment().format('MMMM Do YYYY, h:mm:ss a');
-  // $("#current").html(current);
-
-  function update() {
-      $('#current').html(moment().format('D. MMMM YYYY H:mm:ss'));
-  }
-  setInterval(update, 1000);
-
-
-
-  // Initialize Firebase
-  // 2. Button for adding Employees
-  $("#addTrain").on("click", function() {
-
-      // grab inputs
-      var train = $("#train").val().trim();
-      var destination = $("#destination").val().trim();
-      var start = moment($("#start").val().trim(), "DD/MM/YY").format("X");
-      var interval = $("#interval").val().trim();
-
-      var newTrain = {
-          train: train,
-          destination: destination,
-          start: start,
-          interval: interval
+  $(document).ready(function() {
+      // Initialize Firebase
+      var config = {
+          apiKey: "AIzaSyAv0YOCWtIxD64X0f1_bbAB08XyHuWH7Jk",
+          authDomain: "trainstation-2b3dd.firebaseapp.com",
+          databaseURL: "https://trainstation-2b3dd.firebaseio.com",
+          storageBucket: "trainstation-2b3dd.appspot.com",
+          messagingSenderId: "393068726925"
+      };
+      firebase.initializeApp(config);
+      // inital varibles
+      // database
+      var database = firebase.database();
+      // train
+      var train = "";
+      var destination = "";
+      var start = "";
+      var interval = 0;
+      // show the current running time
+      function update() {
+          $('#current').html(moment().format('D. MMMM YYYY H:mm:ss'));
       }
+      setInterval(update, 1000);
+      // 2. Button for adding trains
+      $("#addTrain").on("submit", function() {
 
-      database.ref().push(newTrain); // this is new. uploads data to database with push
-
-      // print to console
-      console.log(newTrain.train);
-      console.log(newTrain.destination);
-      console.log(newTrain.start);
-      console.log(newTrain.interval);
-
-      alert("Employee successfully added.");
-
-      // clear all text inputs
-      $("#train").val("");
-      $("#destination").val("");
-      $("#start").val("");
-      $("#interval").val("");
+          // grab inputs
+          train = $("#train").val().trim();
+          destination = $("#destination").val().trim();
+          start = $("#start").val().trim();
+          interval = $("#interval").val().trim();
 
 
-      return false;
+          var newTrain = {
+              train: train,
+              destination: destination,
+              start: start,
+              interval: interval
+          }
+
+          database.ref().push(newTrain); // this is new. uploads data to database with push
+
+          // print to console
+          console.log(newTrain.train);
+          console.log(newTrain.destination);
+          console.log(newTrain.start);
+          console.log(newTrain.interval);
+
+          alert("Train line successfully added.");
+
+          // clear all text inputs
+          $("#train").val("");
+          $("#destination").val("");
+          $("#start").val("");
+          $("#interval").val("");
+
+
+          return false;
+
+      });
+
+      // create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+      database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+          console.log(childSnapshot.val());
+
+          var train = childSnapshot.val().train;
+          var destination = childSnapshot.val().destination;
+          var start = childSnapshot.val().start;
+          var interval = childSnapshot.val().interval;
+          // console log printing
+          console.log(train);
+          console.log(destination);
+          console.log(start);
+          console.log(interval);
+
+
+          var startMoment = moment(childSnapshot.val().start, "hh:mm").subtract(1, "years");
+
+          var diffTime = moment().diff(moment(startMoment), "minutes");
+
+          var remainder = diffTime % childSnapshot.val().interval;
+
+          var wait = childSnapshot.val().interval - remainder;
+
+          var nextTrain = moment().add(wait, "minutes");
+
+          console.log("Train Name: " + childSnapshot.val().train);
+          console.log("Destination: " + childSnapshot.val().destination);
+          console.log("First Train: " + childSnapshot.val().start);
+          console.log("Frequency: " + childSnapshot.val().interval);
+          console.log("Next Train Time: " + moment(nextTrain).format("hh:mm A"));
+          console.log("Minutes Until: " + wait);
+          console.log("====================");
+
+          // add each data to the table
+          $("#trainTable > tbody").append('<tr><td>' + train + '</td><td>' + destination + '</td><td>' + interval + '</td><td>' + wait + '</td><td>' + nextTrain + '</td></tr>');
+      }, function(errorObject) {
+
+          console.log("The read failed: " + errorObject.code);
+
+      });
+
 
   });
-
-  // create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-      console.log(childSnapshot.val());
-
-      var train = childSnapshot.val().train;
-      var destination = childSnapshot.val().destination;
-      var start = childSnapshot.val().start;
-      var interval = childSnapshot.val().interval;
-      // console log printing
-      console.log(train);
-      console.log(destination);
-      console.log(start);
-      console.log(interval);
-
-      // let's prettify
-      var startPretty = moment.unix(start).format("MM/DD/YY");
-      // calculate months worked
-      var wait = moment().diff(moment.unix(start, 'X'), "months");
-      console.log(wait);
-
-      // add each data to the table
-      $("#trainTable > tbody").append('<tr><td>' + train + '</td><td>' + destination + '</td><td>' + interval + '</td><td>' + startPretty + '</td><td>' + wait + '</td></tr>');
-  });
+  // wait time is the time difference between current time and arrival time
+  // current time is already running in html
+  //arrival time is the start time plus interval time
